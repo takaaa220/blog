026684bfb0d7dab25829891b8488e0ad10e4e9ed
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-05-26 18:07:22 +0900

    create post table

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    title {"初めてのBlog"}
+    content {"いい感じ！"}
+  end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before do
+    @post = build(:post)
+  end
+
+  describe "validation" do
+    it do
+      expect(post).to be_valid
+    end
+
+    describe "#title" do
+      it "is invalid without title" do
+        @post.title = ""
+        expect(post).not_to be_valid
+      end
+
+      it "is invalid when title > 50" do
+        @post.title = "a" * 51
+        expect(post).not_to be_valid
+      end
+    end
+
+    describe "#content" do
+      it "is invalid without content" do
+        @post.content = ""
+        expect(post).not_to be_valid
+      end
+    end
+  end
+end
