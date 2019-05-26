5655067ef287ea7ce8c51862a943d584d89dd7c7
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-05-26 18:25:39 +0900

    ja and post

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'carrierwave'
 gem 'draper'
 gem 'ransack'
 gem 'kaminari'
+gem 'rails-i18n'
 # gem 'capistrano-rails', group: :development
 
 # Reduces boot times through caching; required in config/boot.rb
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,25 +7,25 @@ RSpec.describe Post, type: :model do
 
   describe "validation" do
     it do
-      expect(post).to be_valid
+      expect(@post).to be_valid
     end
 
     describe "#title" do
       it "is invalid without title" do
         @post.title = ""
-        expect(post).not_to be_valid
+        expect(@post).not_to be_valid
       end
 
       it "is invalid when title > 50" do
         @post.title = "a" * 51
-        expect(post).not_to be_valid
+        expect(@post).not_to be_valid
       end
     end
 
     describe "#content" do
       it "is invalid without content" do
         @post.content = ""
-        expect(post).not_to be_valid
+        expect(@post).not_to be_valid
       end
     end
   end
