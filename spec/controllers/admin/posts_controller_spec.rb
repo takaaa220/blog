7db9ab17c3604bb64330a2e6f2ef7826e5b6e35e
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-06-02 11:05:43 +0900

    add spec

--- spec/controllers/admin/posts_controller_spec.rb
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -18,4 +18,38 @@ RSpec.describe Admin::PostsController, type: :controller do
       expect(response).to have_http_status 200
     end
   end
+
+  describe "#toggle" do
+    before do
+      @user = create(:user, { status: "admin"} )
+      login_as(@user)
+    end
+
+    it "raises without post" do
+      expect {
+        post :toggle, params: { id: 10000 }
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "can publish post" do
+      @post = create(:post)
+      expect{
+        post :toggle, params: { id: @post.id }
+      }.to change { Post.last.published }.from(false).to(true)
+    end
+
+    it "can unpublished post" do
+      @post = create(:post, { published: true} )
+      expect {
+        post :toggle, params: { id: @post.id }
+      }.to change { Post.last.published }.from(true).to(false)
+    end
+
+    it "redirect_to admin_posts_path" do
+      @post = create(:post)
+      post :toggle, params: { id: @post.id }
+      expect(response).to have_http_status 302
+      expect(response).to redirect_to admin_posts_path
+    end
+  end
 end
