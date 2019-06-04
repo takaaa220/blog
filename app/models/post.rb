90f6b7a8e57561f03bf32191b72bd7cd8022dfe0
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-06-04 21:23:18 +0900

    published callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,17 @@ class Post < ApplicationRecord
   scope :desc, -> { order(id: :desc) }
   scope :published, -> { where(published: true) }
 
+  before_update :changed_published_status
+
   def self.posts_per_month
     Post.group("MONTH(date)")
+  end
+
+  private
+
+  def changed_published_status
+    if published_changed? && published_at.nil? && published?
+      self.published_at = Time.zone.now
+    end
   end
 end
