e4fd966a79a9b5fd62eadcdf89f2d5a423d64a93
takaaa220 <takaaaaaaaaaa220@gmail.com>
2019-06-22 11:35:20 +0900

    fix toggle path

--- app/decorators/admin/post_decorator.rb
+++ app/decorators/admin/post_decorator.rb
@@ -3,6 +3,6 @@ class Admin::PostDecorator < ApplicationDecorator
 
   def toggle_button
     button = published? ? "非公開" : "公開"
-    helpers.button_to(button, helpers.toggle_admin_post_path(self), data: { confirm: "OKですか？"}, class: "Button" )
+    helpers.button_to(button, helpers.toggle_admin_post_path(self.pid), data: { confirm: "OKですか？"}, class: "Button" )
   end
 end
